e39a70ad04b060ce71f08aac9c44c4dca932503d
VictorAmaya99 <vittoriomanuel99@gmail.com>
2025-11-30 18:52:13 -0300

    cambios necesarios

--- js/main.js
+++ js/main.js
@@ -1,46 +1,223 @@
-let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-document.querySelectorAll('.agregar-carrito').forEach(btn => {
-    btn.addEventListener('click', () =>{
-        const producto = {
-            nombre: btn.parentElement.querySelector('h3').innerText,
-            precio: parseInt(btn.parentElement.querySelector('p').innerText.replace('$', ''))
-        };
-        carrito.push(producto)
-        localStorage.setItem('carrito', JSON.stringify(carrito));
-        alert(`${producto.nombre} agregado al carrito`);
-        actualizarCarrito();
-    });
-})
+/* Variables DOM*/
 
-function actualizarCarrito(){
-    const carritoDiv = document.getElementById('carrito-items');
-    const totalSpan = document.getElementById('total');
-    if(!carritoDiv) return;
+let productos = [];
 
-    carritoDiv.innerHTML = '';
-    let total = 0;
-    carrito.forEach(p => {
-        carritoDiv.innerHTML += `<p>${p.nombre} - $${p.precio}</p>`;
-        total += p.precio;
-    });
-    totalSpan.innerText = total;
+let contenedorProductos = document.getElementById("contenedorProductos")
+let barraBusqueda = document.getElementById("barraBusqueda");
+let contenedorCarrito = document.getElementById("contenedorCarrito");
+let contenedorVaciarPrecioTotal = document.getElementById("contenedorVaciarPrecioTotal");
+let contadorCarrito = document.getElementById("contadorCarrito");
+let ordenarPorNombre = document.getElementById("ordenarPorNombre");
+let ordenarPorPrecio = document.getElementById("ordenarPorPrecio");
+
+let carrito = [];
+
+
+/*==================
+    FUNCIONES
+====================*/
+// Funcion para cargar los productos
+async function cargarProductos() {
+    try {
+        let response = await fetch("json/productos.json");
+        productos = await response.json();
+        mostrarProductos(productos);
+        console.table(productos);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-actualizarCarrito();
-
-document.addEventListener("DOMContentLoaded", () => {
-    //cargar header
-    fetch('../frontend/partials/header.html')
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById('header-container').innerHTML = data;
+// Funcion para mostrar los productos
+function mostrarProductos(array){
+    let cardProducto = "";
+    array.forEach(a => {
+        cardProducto += `
+        <div class="card-producto">
+            <img src="${a.rutaImg}" alt="${a.nombre}">
+            <h3>${a.nombre}</h3>
+            <p>$${a.precio}</p>
+            <button onclick="agregarItemCarrito(${a.id})">Agregar al carrito</button>
+        </div>
+        `;
     });
 
-    //cargar footer
-    fetch('../frontend/partials/footer.html')
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById('footer-container').innerHTML = data;
+    contenedorProductos.innerHTML = cardProducto;
+}
+
+// Funcion para manejar la busqueda y filtracion de productos
+
+barraBusqueda.addEventListener("keyup", filtrarProd);
+
+function filtrarProd(){
+    let busqueda = barraBusqueda.value.trim().toLocaleLowerCase();
+
+    prodFiltrados = productos.filter(p => p.nombre.toLocaleLowerCase().includes(busqueda));
+
+    mostrarProductos(prodFiltrados);
+}
+
+/*========================================================
+    Funcion para implementar al carrito
+==========================================================*/
+
+// Agregar item al carrito
+function agregarItemCarrito(id){
+    let prodSelected = productos.find(p => p.id === id);
+    carrito.push(prodSelected);
+
+    console.table(prodSelected);
+
+    guardarLocalStorage();
+    visualizarCarrito();
+    vaciarPrecioTotal()
+    contadorProd();
+    
+}
+
+//Mostrar los productos seleccionados en el carrito
+
+function visualizarCarrito(){
+    let cardCarrito = "<ul>";    
+    carrito.forEach((e, indice) =>{          
+        cardCarrito += `        
+        <li class="bloque-item">
+            <img src="${e.rutaImg}" alt="${e.nombre}">
+            <p class="nombre-item">${e.nombre} - ${e.precio}</p>
+            <button class="boton-eliminar" onclick="eliminarProducto(${indice})">Eliminar</button>
+        </li>
+        `;
     });
+
+    cardCarrito += "</ul>"
+    contenedorCarrito.innerHTML = cardCarrito;
+
+    vaciarPrecioTotal();
+}
+
+// Manejar el boton vaciar del carrito y el precio total
+
+function vaciarPrecioTotal(){
+    //usamos reduce para sumar el precio y presentar el total
+    let total = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+
+    // inicialiamos en 0 para aparezca cuando hay un producto
+    if(carrito.length > 0){
+        let cardVaciarPrecioTotal = `
+        <div class="vaciar-precioTotal">
+            <button class="boton-vaciar" onclick="vaciarCarrito()">Vaciar carrito</button>
+            <p class="total-precio">Total: ${total}</p>
+        </div>
+        `;
+
+        contenedorVaciarPrecioTotal.innerHTML = cardVaciarPrecioTotal;
+    } else{
+        contenedorVaciarPrecioTotal.innerHTML = "";
+    }
+}    
+
+//Eliminar el producto seleccionado
+
+function eliminarProducto(indice){
+    carrito.splice(indice, 1);
+
+    guardarLocalStorage();
+    visualizarCarrito();
+    contadorProd();
+
+}
+
+// Funcion que sirve como contador de productos agregados al carrito
+
+function contadorProd(){
+    contadorCarrito.innerHTML = `Carrito: <span class="cantidadProductos">${carrito.length}</span> productos`;
+}
+
+// Vaciar carrito
+
+function vaciarCarrito(){
+    carrito = [];
+    contenedorCarrito.innerHTML = "";
+    
+    guardarLocalStorage();
+    contadorProd();
+    vaciarPrecioTotal();
+}
+
+/*========================================================
+    Funciones para manejar localStorage
+==========================================================*/
+
+// Guardar productos seleccionados al local storage
+
+function guardarLocalStorage(){
+    localStorage.setItem("carrito", JSON.stringify(carrito)); //"carrito", representan los productos agregados
+    // Con JSON.stringify se convierte el array en una cadena Json.
+}
+
+// Cargar localStorage
+
+function loadLocalStorage(){
+    let datos = localStorage.getItem("carrito"); //Aca se trae el producto guardado
+
+    // Si datos es verdadero entonces el carrito se convierte el json en un array de objetos, sino muestra un carrito vacio
+    if(datos){
+        carrito = JSON.parse(datos);
+    } else {
+        carrito = [];
+    }
+}
+
+/*========================================================
+    Funciones que manejan el ordenamiento de los 
+    productos
+==========================================================*/
+
+ordenarPorNombre.addEventListener("click", ordenarNombre);
+ordenarPorPrecio.addEventListener("click", ordenarPrecioDesc);
+
+function ordenarPrecioDesc(){
+    productos.sort((a, b) => a.precio - b.precio);
+    mostrarProductos(productos);
+}
+
+function ordenarNombre(){
+    productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+    mostrarProductos(productos);
+}
+
+/*================== 
+   DRAWER CARRITO 
+====================*/
+
+// botón en el header
+let btnCarrito = document.getElementById("btnCarrito");
+
+// aside lateral
+let drawerCarrito = document.getElementById("drawerCarrito");
+
+//cerrar el carrito
+let cerrarDrawer = document.querySelector("#cerrarDrawer");
+
+// abrir
+btnCarrito.addEventListener("click", (e) => {
+    e.preventDefault();
+    drawerCarrito.classList.add("open");
 });
+
+// cerrar tocando afuera del drawer
+cerrarDrawer.addEventListener("click", () => {
+    drawerCarrito.classList.remove("open");
+});
+
+
+
+async function init() {
+    loadLocalStorage();
+    await cargarProductos();
+    visualizarCarrito();
+    contadorProd();
+}
+
+init();
