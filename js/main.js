3f9791708e8f39c4e00ad76961765f3c14de9b27
VictorAmaya99 <vittoriomanuel99@gmail.com>
2025-12-03 20:02:47 -0300

    agregados

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Redireccion a inicio////////////////////
+let nombreUsuario = sessionStorage.getItem("nombreUsuario");
+
+// Redirige si no existe un nombre de usuario
+if(!nombreUsuario){
+	window.location.href = "index.html";
+}
+
+
 
 /* Variables DOM*/
 
@@ -11,6 +20,8 @@ let contenedorVaciarPrecioTotal = document.getElementById("contenedorVaciarPreci
 let contadorCarrito = document.getElementById("contadorCarrito");
 let ordenarPorNombre = document.getElementById("ordenarPorNombre");
 let ordenarPorPrecio = document.getElementById("ordenarPorPrecio");
+
+let boton_imprimir = document.getElementById("btn-imprimir");
 
 let carrito = [];
 
@@ -52,20 +63,7 @@ function mostrarProductos(array){
             <button class="botonera-agregar-carrito" onclick="agregarItemCarrito(${array[i].id})">Agregar al carrito</button>
         </div>
         `;
-    }    
-    
-
-
-    // array.forEach(a => {
-    //     cardProducto += `
-    //     <div class="card-producto">
-    //         <img src="${a.rutaImg}" alt="${a.nombre}">
-    //         <h3>${a.nombre}</h3>
-    //         <p>$${a.precio}</p>
-    //         <button class="botonera-agregar-carrito" onclick="agregarItemCarrito(${a.id})">Agregar al carrito</button>
-    //     </div>
-    //     `;
-    // });
+    }      
 
     contenedorProductos.innerHTML = cardProducto;
 }
@@ -80,6 +78,158 @@ function filtrarProd(){
     prodFiltrados = productos.filter(p => p.nombre.toLocaleLowerCase().includes(busqueda));
 
     mostrarProductos(prodFiltrados);
+}
+
+/*========================================================
+    Funcion para imprimir tickets pdf
+==========================================================*/
+
+boton_imprimir.addEventListener("click", imprimirTicket);
+
+function imprimirTicket() {
+    console.table(carrito);
+    
+    // Para registrar las ventas a posteriori, guardamos los ids de los productos del carrito
+    let idProductos = []; // Array vacío de ids de productos
+
+    // Gracias al CDN extraemos la clase jspdf del objeto global window  
+    const { jsPDF } = window.jspdf;
+
+    // Creamos una nueva instancia del documento pdf usando al clase jsPDF
+    const doc = new jsPDF(); // Ahora doc tendrá todos los métodos que le provee la herramienta jsPDF
+
+    // Definimos el margen superior de 20px en el eje y -> eje vertical, el eje x será el eje horizontal
+    let y = 20;
+
+    // Establecemos el tamaño de 18px para el primer texto
+    doc.setFontSize(18);
+
+    // Escribimos el texto "Ticket compra" en la posicion x=10, y=10 del pdf
+    doc.text("ticket de compra:", 20, y);
+
+
+    // Aumentamos el espacio despues del titulo
+    y += 20;
+
+      // Cambiamos el tamaño de la fuente a 12px para los productos del ticket
+    doc.setFontSize(12);
+
+    // Iteramos el carrito e imprimimos nombre y precio
+    carrito.forEach(producto => {
+
+        idProductos.push(producto.id); // Llenamos el array de ids de productos (necesario para la venta despues)
+
+        doc.text(`${producto.nombre} - $${producto.precio}`, 30, y); // Creamos el texto por cada producto: nombre = $precio
+
+        // Incrementamos la posicion vertical para evitar solapamiento
+        y += 10;
+    });
+
+     // Calculamos el total del ticket usando reduce
+    const precioTotal = carrito.reduce((total, producto) => total + parseInt(producto.precio), 0);
+
+     // Añadimos otro espacio de 5px en el eje vertical para separar el precio total de los productos
+    y += 5;
+
+    // Establecemos el tamaño de 15px para el precio total
+    doc.setFontSize(14);
+
+    // Escribimos el total del ticket en el PDF, despues del listado de productos
+    doc.text(`Total: $${precioTotal}`, 20, y);
+
+    // Imprimimos el ticket de venta
+    doc.save("ticket.pdf");
+
+    //Lamado a registrar ventas y que haga la redirección -> fetch POST /api/sales -> luego crearemos este endpoint app.post("/api/sales")
+    registrarVenta(precioTotal, idProductos); 
+
+}
+
+// Registrar venta ////////////////////////////////////////
+/* Que datos debemos enviar a nuestra API?
+
+Nuestro endpoint esperará algo equivalente a los campos de la tabla sales
+    - date          nueva fecha
+    - total_price   precio total
+    - user_name     nombreUsuario
+
+Además nuestra tabla product_sales vincula los productos a la venta, por tanto enviaremos los ids de los produtos vendidos
+    - products      array de ids de productos -> vinculamos los productos a una venta
+
+
+Ejemplo del JSON
+    {
+        "date": "2025-12-03T10:00",
+        "total_price" : "6500",
+        "user_name" : "Gricel",
+        "products" : [29, 27]
+    }
+
+Que tendra que hacer ahora nuestra API?
+
+    1. Insertar la venta en la tabla sales
+    2. Despues de esta insercion, obtendremos el id de la venta
+    3. Insertamos los productos en product_sales
+*/
+
+async function registrarVenta(precioTotal, idProductos) {
+
+     /* toLocaleString vs toISOString
+
+        - Los métodos `toLocaleString()` y `toISOString()` de JavaScript tienen diferentes propósitos a la hora de convertir un objeto Date en una cadena. El método `toISOString()` siempre devuelve una cadena en formato ISO 8601, que representa la fecha y la hora en UTC (tiempo universal coordinado) e incluye una «Z» al final para indicar UTC. Este formato está estandarizado y es coherente independientemente de la configuración del sistema del usuario.
+
+        - Por el contrario, `toLocaleString()` devuelve una cadena formateada según la configuración regional y la zona horaria del sistema del usuario o según lo especificado por los parámetros del método. Esto significa que el resultado puede variar significativamente en función de la ubicación del usuario, por ejemplo, utilizando diferentes separadores de fecha, formatos de hora o incluso diferentes nombres de días y meses. Por ejemplo, si se utiliza la configuración regional «de» (alemán), la fecha se formateará como «29.5.2020, 18:04:24», mientras que «fr» (francés) utilizará «29/05/2020, 18:04:24».
+
+        - Una solución habitual para obtener la hora local en formato ISO 8601 (sin la «Z») es ajustar la fecha según la diferencia horaria antes de llamar a «toISOString()». Esto se puede hacer restando la diferencia horaria en milisegundos (obtenida mediante «getTimezoneOffset () * 60000») del valor de la hora de la fecha. A continuación, la cadena resultante se puede modificar para eliminar la «Z» final si es necesario. Alternativamente, el uso de una configuración regional como «sv» (Suecia) con «toLocaleString()» produce un formato similar al ISO 8601, aunque utiliza un espacio en lugar de «T» entre la fecha y la hora, lo que sigue siendo válido según la RFC 3339.
+    */
+
+    // Ya que el formato fecha no es valido para timestamp en SQL, tenemos que formatearlo    
+    const fecha = new Date()
+    .toLocaleString("sv-SE", { hour12: false })  
+    .replace("T", " ");
+
+    console.log(fecha);
+
+     // Construimos el objeto con informacion para mandarle al endpoint (previo parseo a JSON)
+    const data = {
+        // date: fecha, // Recordar que si en su BBDD tienen un valor generado automaticamente, no hace falta enviar esto}
+        precio_total: precioTotal,
+        nombre_usuario: nombreUsuario,
+        productos: idProductos
+    }
+
+    // Endpoint ventas
+    const response = await fetch("http://localhost:3000/api/ventas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+     const result = await response.json();
+
+      if(response.ok) {
+        console.log(response);
+        alert(result.message);
+
+        // Limpieza de variables en sesion y redireccion para resetear la app
+        sessionStorage.removeItem("nombreUsuario");
+        // sessionStorage.removeItem("carrito"); // Si guardamos el carrito en session
+        window.location.href = "index.html"
+    } else {
+        alert(result.message);
+    }
+
+
+
+     /*
+    // Una vez que terminasemos de registrar la venta -> ORDEN IDEAL 1. Venta -> 2. Ticket
+    alert("Venta creada con exito");
+    sessionStorage.removeItem("nombreUsuario");
+    // sessionStorage.removeItem("carrito"); // Si guardamos el carrito en session
+    window.location.href = "index.html"
+    */
 }
 
 /*========================================================
